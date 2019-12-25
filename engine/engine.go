3fe873af8d98c30678c18fd9b91d621c64be5c36
masha-dubyna <mashamartushenko@gmail.com>
2019-12-25 12:15:52 +0200

    Solve synchronization problem in engine.go

--- engine/engine.go
+++ engine/engine.go
@@ -1,5 +1,7 @@
 package engine
 
+import "sync"
+
 type Command interface {
 	Execute(handler Handler)
 }
@@ -8,30 +10,36 @@ type Handler interface {
 	Post(cmd Command)
 }
 
+var isFinished = make(chan bool)
+
 type EventLoop struct {
+	sync.Mutex
 	messageQueue []Command
-	isFinished   chan (bool)
 	Await        bool
 }
 
 func (eventloop *EventLoop) Start() {
-	eventloop.isFinished = make(chan bool)
 	go func() {
 		for !eventloop.Await {
 		}
 		for i := 0; i < len(eventloop.messageQueue); i++ {
+			eventloop.Lock()
 			cmd := eventloop.messageQueue[i]
+			eventloop.Unlock()
 			cmd.Execute(eventloop)
 		}
-		eventloop.isFinished <- true
+		isFinished <- true
 	}()
 }
 
 func (eventloop *EventLoop) Post(cmd Command) {
+	eventloop.Lock()
 	eventloop.messageQueue = append(eventloop.messageQueue, cmd)
+	eventloop.Unlock()
+
 }
 
 func (eventloop *EventLoop) AwaitFinish() {
 	eventloop.Await = true
-	<-eventloop.isFinished
+	<- isFinished
 }
